346141222301be86a5e0f012abb2b60c8aeed40d
Kai Summers <kaisummers@users.noreply.github.com>
2018-03-14 02:48:30 +0800

    Update classes.php

--- classes.php
+++ classes.php
@@ -4,28 +4,26 @@
  */
 class base62
 {
-  // Base62 Encode
-  public function encode($d)
-  {
-	  $l = strlen($d);
-	  for ($i = 0; $i < $l; $i += 8) 
-    {
-      $c = substr($d, $i, 8);
-		  $p = str_pad(gmp_strval(gmp_init(ltrim(bin2hex($c), '0'), 16), 62), ceil((strlen($c) * 8)/6), '0', STR_PAD_LEFT);
-		  $o[] = $p;
-	  }
-	  return implode($o);
-  }
-  // Base62 Decode
-  function decode($d)
-  {
-	  $l = strlen($d);
-	  for ($i = 0; $i < $l; $i += 11) 
-    {
-		  $c = substr($d, $i, 11);
-		  $p = str_pad(gmp_strval(gmp_init(ltrim($c, '0'), 62), 16), floor((strlen($c) * 6)/8) * 2, '0', STR_PAD_LEFT);
-		  $o[] = pack('H*', $p);
-	  }
-	  return implode($o);
-  }
+  	// Base62 Encode
+	public function encode($d)
+	{
+		$l = strlen($d);
+		for ($i = 0; $i < $l; $i += 8) 
+		{
+			$c = substr($d, $i, 8);
+			$o[] = str_pad(gmp_strval(gmp_init(ltrim(bin2hex($c), '0'), 16), 62), ceil((strlen($c) * 8)/6), '0', STR_PAD_LEFT);
+	 	}
+	 	return implode($o);
+  	}
+  	// Base62 Decode
+  	public function decode($d)
+  	{
+		$l = strlen($d);
+		for ($i = 0; $i < $l; $i += 11) 
+    		{
+			$c = substr($d, $i, 11);
+			$o[] = pack('H*', str_pad(gmp_strval(gmp_init(ltrim($c, '0'), 62), 16), floor((strlen($c) * 6)/8) * 2, '0', STR_PAD_LEFT));
+		}
+		return implode($o);
+	}
 }
